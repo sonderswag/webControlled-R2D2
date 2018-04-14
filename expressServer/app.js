ec78b1eedafa2057efdcf3022585951ccc28394a
sonderswag <csondergardwagner@gmail.com>
2018-04-14 15:31:25 -0700

    test on rpi

--- expressServer/app.js
+++ expressServer/app.js
@@ -1,6 +1,8 @@
-import express from 'express';
+const express = require('express');
 const app = express()
+
+const port = 4200;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(4200, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
